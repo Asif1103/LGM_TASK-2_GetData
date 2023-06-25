ad663c0b8af61bcf7308cc2d3aaf00be2809c18d
Asif Shaik <124784024+Asif1103@users.noreply.github.com>
2023-06-25 20:15:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import { Component } from 'react';
+import './App.css';
+import SearchPage from './components/searchPage';
+
+function App(){
+  return(
+    <>
+      <searchPage/>
+    </>
+  )
+}
+
+export default App;
